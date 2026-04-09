368c14281786184f783591fb441d35a7af3e841a
ep3264 <portsup9@gmail.com>
2026-04-09 21:27:51 +0300

    Add: course-schedule - submission-9

---
+++ Data Structures & Algorithms/course-schedule/submission-9.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number} numCourses
+     * @param {number[][]} prerequisites
+     * @return {boolean}
+     */
+    canFinish(numCourses, prerequisites) {
+        let b = true;
+
+        for (let i = 0; i < prerequisites.length; i++) {
+            b = b && this.dfs(prerequisites[i][1], new Set(), prerequisites);
+        }
+
+        return b;
+    }
+
+    dfs(i, visited, prerequisites) {
+        if (visited.has(i)) return false;
+        visited.add(i);
+        let b = true;
+        for (let j = 0; j < prerequisites.length; j++) {
+            if (prerequisites[j][0] == i) {
+                if(!this.dfs(prerequisites[j][1], visited, prerequisites)) return false;
+            }
+        }
+
+        return b;
+    }
+}
